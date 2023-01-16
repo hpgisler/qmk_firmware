6b86765f91348a7121030e48a39fd60d1907f8df
Hanspeter Gisler <hanspeter.gisler@gisler.pro>
2023-01-16 11:36:47 +0100

    put enter KC under right pinky

--- keyboards/crkbd/keymaps/hpgisler-callum/keymap.c
+++ keyboards/crkbd/keymaps/hpgisler-callum/keymap.c
@@ -124,8 +124,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      // qQ       wW       eE       rR       tT       zZ       uU       iI       oO       pP
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    CH_Z,    KC_U,    KC_I,    KC_O,    KC_P,
 
-     // aA       sS       dD       fF       gG       hH       jJ       kK       lL       nN
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+     // aA       sS       dD       fF       gG       hH       jJ       kK       lL
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
 
      // yY       xX       cC       vV       bB       nN       mM       ,;       .:       -_ 
         CH_Y,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  CH_MINS,
