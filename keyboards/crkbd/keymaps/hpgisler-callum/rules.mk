8f999fbb280849c5a56232cb9487f21e51dc962c
Hanspeter Gisler <hanspeter.gisler@gisler.pro>
2023-01-28 14:31:04 +0100

    remove unsupported comments format

--- keyboards/crkbd/keymaps/hpgisler-callum/rules.mk
+++ keyboards/crkbd/keymaps/hpgisler-callum/rules.mk
@@ -2,7 +2,6 @@ MOUSEKEY_ENABLE     = yes
 OLED_DRIVER         = SSD1306
 LTO_ENABLE          = yes
 KEY_OVERRIDE_ENABLE = yes
-// CAPS_WORD_ENABLE    = yes
 COMBO_ENABLE        = yes
 
 SRC += oneshot.c
