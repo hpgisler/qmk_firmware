8693ea6fedc7d1ef3998005eaf0da54f6add4327
Hanspeter Gisler <hanspeter.gisler@gisler.pro>
2023-03-17 13:38:59 +0100

    change back again to one-shift and one 2nd layer switch layout

--- keyboards/crkbd/keymaps/hpgisler/keymap.c
+++ keyboards/crkbd/keymaps/hpgisler/keymap.c
@@ -90,15 +90,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                            LSFT_T(KC_I)   , LT(NUM0,KC_S)  , LT(SYM0,KC_T)  , LT(SYS0 ,KC_R) , LT(SYS0, KC_N) , LT(SYM0, KC_E) , LT(NUM0,KC_A)  , RSFT_T(KC_C)   ,
 
-                                            XXXXXXX       ,LT(ALA1, KC_SPC) , REPEAT         , OSL(ALA3)      , OSL(ALA2)      , XXXXXXX
+                                            XXXXXXX        ,KC_SPC          , OSL(ALA1)      , REPEAT         , OSL(ALA2)      , XXXXXXX
                            ),
 
     [ALA1] = LAYOUT_split_10(
                                             LSFT(KC_L)     , LSFT(KC_D)     , LSFT(KC_G)     , LSFT(KC_H)     , LSFT(KC_U)     , LSFT(CH_O)     ,
 
                            LSFT(KC_I)     , LSFT(KC_S)     , LSFT(KC_T)     , LSFT(KC_R)     , LSFT(KC_N)     , LSFT(KC_E)     , LSFT(KC_A)     , LSFT(CH_C)     ,
-
-                                            _______        , _______        , _______        , _______        , _______        , _______
+           
+                                            _______        , _______        , _______        , _______        , MO(ALA3)                                                           , _______
+                                            //_______        , _______        , _______        , _______        , OSL(ALA3)      , _______
                            ),
     
     [ALA2] = LAYOUT_split_10(
@@ -106,7 +107,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                            KC_Q           , KC_J           , LCTL_T(KC_K)   , LALT_T(KC_P)   , RALT_T(KC_B)   , LCTL_T(CH_DOT) , KC_X           , CH_Y           ,
 
-                                            _______        , _______        , _______        , _______        , MO(ALA3)       , _______                        
+                                            //_______        , _______        , OSL(ALA3)       , _______        , _______        , _______                        
+                                            _______        , _______        , MO(ALA3)       , _______        , _______        , _______                        
                            ),
  
     [ALA3] = LAYOUT_split_10(
